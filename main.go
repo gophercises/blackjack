54494c88464b8e6da9b146110f72ed43834358a8
Jon Calhoun <joncalhoun@gmail.com>
2018-03-09 16:42:19 -0500

    completed part1 - the basic game

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/gophercises/deck"
+)
+
+type Hand []deck.Card
+
+func (h Hand) String() string {
+	strs := make([]string, len(h))
+	for i := range h {
+		strs[i] = h[i].String()
+	}
+	return strings.Join(strs, ", ")
+}
+
+func (h Hand) DealerString() string {
+	return h[0].String() + ", **HIDDEN**"
+}
+
+func main() {
+	cards := deck.New(deck.Deck(3), deck.Shuffle)
+	var card deck.Card
+	var player, dealer Hand
+	for i := 0; i < 2; i++ {
+		for _, hand := range []*Hand{&player, &dealer} {
+			card, cards = draw(cards)
+			*hand = append(*hand, card)
+		}
+	}
+	var input string
+	for input != "s" {
+		fmt.Println("Player:", player)
+		fmt.Println("Dealer:", dealer.DealerString())
+		fmt.Println("What will you do? (h)it, (s)tand")
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			card, cards = draw(cards)
+			player = append(player, card)
+		}
+	}
+	fmt.Println("==FINAL HANDS==")
+	fmt.Println("Player:", player)
+	fmt.Println("Dealer:", dealer)
+}
+
+func draw(cards []deck.Card) (deck.Card, []deck.Card) {
+	return cards[0], cards[1:]
+}
