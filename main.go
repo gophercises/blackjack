81fed704e4190a8e5460a55f7f737826de5118f9
Jon Calhoun <joncalhoun@gmail.com>
2018-03-13 23:23:08 -0400

    added the GameState type

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/gophercises/deck"
@@ -52,51 +51,90 @@ func min(a, b int) int {
 }
 
 func main() {
-	cards := deck.New(deck.Deck(3), deck.Shuffle)
-	var card deck.Card
-	var player, dealer Hand
-	for i := 0; i < 2; i++ {
-		for _, hand := range []*Hand{&player, &dealer} {
-			card, cards = draw(cards)
-			*hand = append(*hand, card)
-		}
-	}
-	var input string
-	for input != "s" {
-		fmt.Println("Player:", player)
-		fmt.Println("Dealer:", dealer.DealerString())
-		fmt.Println("What will you do? (h)it, (s)tand")
-		fmt.Scanf("%s\n", &input)
-		switch input {
-		case "h":
-			card, cards = draw(cards)
-			player = append(player, card)
-		}
-	}
-	// If dealer score <= 16, we hit
-	// If dealer has a soft 17, then we hit.
-	for dealer.Score() <= 16 || (dealer.Score() == 17 && dealer.MinScore() != 17) {
-		card, cards = draw(cards)
-		dealer = append(dealer, card)
-	}
-	pScore, dScore := player.Score(), dealer.Score()
-	fmt.Println("==FINAL HANDS==")
-	fmt.Println("Player:", player, "\nScore:", pScore)
-	fmt.Println("Dealer:", dealer, "\nScore:", dScore)
-	switch {
-	case pScore > 21:
-		fmt.Println("You busted")
-	case dScore > 21:
-		fmt.Println("Dealer busted")
-	case pScore > dScore:
-		fmt.Println("You win!")
-	case dScore > pScore:
-		fmt.Println("You lose")
-	case dScore == pScore:
-		fmt.Println("Draw")
-	}
+	// cards := deck.New(deck.Deck(3), deck.Shuffle)
+	// var card deck.Card
+	// var player, dealer Hand
+	// for i := 0; i < 2; i++ {
+	// 	for _, hand := range []*Hand{&player, &dealer} {
+	// 		card, cards = draw(cards)
+	// 		*hand = append(*hand, card)
+	// 	}
+	// }
+	// var input string
+	// for input != "s" {
+	// 	fmt.Println("Player:", player)
+	// 	fmt.Println("Dealer:", dealer.DealerString())
+	// 	fmt.Println("What will you do? (h)it, (s)tand")
+	// 	fmt.Scanf("%s\n", &input)
+	// 	switch input {
+	// 	case "h":
+	// 		card, cards = draw(cards)
+	// 		player = append(player, card)
+	// 	}
+	// }
+	// // If dealer score <= 16, we hit
+	// // If dealer has a soft 17, then we hit.
+	// for dealer.Score() <= 16 || (dealer.Score() == 17 && dealer.MinScore() != 17) {
+	// 	card, cards = draw(cards)
+	// 	dealer = append(dealer, card)
+	// }
+	// pScore, dScore := player.Score(), dealer.Score()
+	// fmt.Println("==FINAL HANDS==")
+	// fmt.Println("Player:", player, "\nScore:", pScore)
+	// fmt.Println("Dealer:", dealer, "\nScore:", dScore)
+	// switch {
+	// case pScore > 21:
+	// 	fmt.Println("You busted")
+	// case dScore > 21:
+	// 	fmt.Println("Dealer busted")
+	// case pScore > dScore:
+	// 	fmt.Println("You win!")
+	// case dScore > pScore:
+	// 	fmt.Println("You lose")
+	// case dScore == pScore:
+	// 	fmt.Println("Draw")
+	// }
 }
 
 func draw(cards []deck.Card) (deck.Card, []deck.Card) {
 	return cards[0], cards[1:]
+}
+
+type State int8
+
+const (
+	StatePlayerTurn State = iota
+	StateDealerTurn
+	StateHandOver
+)
+
+type GameState struct {
+	Deck   []deck.Card
+	State  State
+	Player Hand
+	Dealer Hand
+}
+
+func (gs *GameState) CurrentPlayer() *Hand {
+	switch gs.State {
+	case StatePlayerTurn:
+		return &gs.Player
+	case StateDealerTurn:
+		return &gs.Dealer
+	default:
+		panic("it isn't currently any player's turn")
+	}
+}
+
+func clone(gs GameState) GameState {
+	ret := GameState{
+		Deck:   make([]deck.Card, len(gs.Deck)),
+		State:  gs.State,
+		Player: make(Hand, len(gs.Player)),
+		Dealer: make(Hand, len(gs.Dealer)),
+	}
+	copy(ret.Deck, gs.Deck)
+	copy(ret.Player, gs.Player)
+	copy(ret.Dealer, gs.Dealer)
+	return ret
 }
